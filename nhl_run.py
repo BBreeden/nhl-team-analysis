69e7b9269c28fa6474512122ab2a9ce6e9d49cf9
Brian Breeden <breedenb@vt.edu>
2019-10-22 10:35:12 -0400

    Added the request to an independent function

--- nhl_run.py
+++ nhl_run.py
@@ -1,7 +1,9 @@
 import json
 import requests
 
-request = json.loads(requests.get('https://statsapi.web.nhl.com/api/v1/schedule?date=2019-10-22').text)
+def get_request():
+    return json.loads(requests.get('https://statsapi.web.nhl.com/api/v1/schedule?date=2019-10-22').text)
+request =get_request()
 away_id = request['dates'][0]['games'][0]['teams']['away']['team']['id']
 home_id = request['dates'][0]['games'][0]['teams']['home']['team']['id']
 print('home:', home_id)
