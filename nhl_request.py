395391c0a59b3bf91daa5d7ac52c7ace97f40940
Brian Breeden <breedenb@vt.edu>
2019-10-22 16:14:51 -0400

    Added a function to fetch the current date and pass it to the request.

--- nhl_request.py
+++ nhl_request.py
@@ -1,5 +1,9 @@
 import json
 import requests
+from datetime import datetime
 
 def get_request():
-    return json.loads(requests.get('https://statsapi.web.nhl.com/api/v1/schedule?date=2019-10-22').text)
+    return json.loads(requests.get('https://statsapi.web.nhl.com/api/v1/schedule?date=' + get_today()).text)
+
+def get_today():
+    return datetime.today().strftime('%Y-%m-%d')
--- nhl_run.py
+++ nhl_run.py
@@ -2,6 +2,7 @@ import requests
 import nhl_request
 
 
+
 request = nhl_request.get_request()
 away_id = request['dates'][0]['games'][0]['teams']['away']['team']['id']
 home_id = request['dates'][0]['games'][0]['teams']['home']['team']['id']
