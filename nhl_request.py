8605c075dc1ac3f0822a91092ddbc35284f5444a
Brian Breeden <breedenb@vt.edu>
2019-10-22 16:35:06 -0400

    Several functions added, now will loop through the games.

--- nhl_request.py
+++ nhl_request.py
@@ -2,8 +2,15 @@ import json
 import requests
 from datetime import datetime
 
+
+'''
+A GET request from the api to get the scheduled NHL games for today.
+'''
 def get_request():
     return json.loads(requests.get('https://statsapi.web.nhl.com/api/v1/schedule?date=' + get_today()).text)
 
+'''
+Returns today's date in the YYYY-MM-DD format.
+'''
 def get_today():
     return datetime.today().strftime('%Y-%m-%d')
--- nhl_run.py
+++ nhl_run.py
@@ -1,10 +1,24 @@
 import requests
 import nhl_request
 
+'''
+Gets the JSON object that returns data associated with the NHL games that take place today.
+'''
+def init():
+    return nhl_request.get_request()
 
+'''
+//TODO
+Still not quite sure what to do here. Right now, all it does it assign the collection of games to a variable.
+'''
+def get_games(req):
+    games = req['dates'][0]['games'] #fetchs all of the games returned
 
-request = nhl_request.get_request()
-away_id = request['dates'][0]['games'][0]['teams']['away']['team']['id']
-home_id = request['dates'][0]['games'][0]['teams']['home']['team']['id']
-print('home:', home_id)
-print('away', away_id)
+    for i, game in enumerate(games): #loops through each game
+        home_id = req['dates'][0]['games'][i]['teams']['home']['team']['id']
+        away_id = req['dates'][0]['games'][i]['teams']['away']['team']['id']
+        print('{} @ {}'.format(away_id, home_id))
+
+if __name__ == '__main__':
+    req = init()
+    get_games(req)
